a8547fc486f5fd232e0bcc72943de0f13f75c7ac
Gaurav Mathur <gauravmathur24@gmail.com>
2016-04-10 00:30:28 +0530

    New Screens Added

---
+++ app/src/main/java/com/gaurav/myreminder/AddReminder.java
@@ -0,0 +1,25 @@
+package com.gaurav.myreminder;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+public class AddReminder extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_reminder);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                finish();
+                break;
+        }
+        return true;
+    }
+}
