e83cb4f2e5c2ba53595e102079a13ec3530811db
Gaurav Mathur <gauravmathur24@gmail.com>
2016-04-10 00:30:55 +0530

    New Screens Added

--- app/src/main/java/com/gaurav/myreminder/LoginPage.java
+++ app/src/main/java/com/gaurav/myreminder/LoginPage.java
@@ -3,6 +3,7 @@ package com.gaurav.myreminder;
 import android.animation.Animator;
 import android.animation.AnimatorListenerAdapter;
 import android.annotation.TargetApi;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.support.annotation.NonNull;
 import android.support.design.widget.Snackbar;
@@ -333,6 +334,9 @@ public class LoginPage extends AppCompatActivity implements LoaderCallbacks<Curs
             showProgress(false);
 
             if (success) {
+                //finish();
+                Intent homeScreen = new Intent(getApplicationContext(),HomeScreen.class);
+                startActivity(homeScreen);
                 finish();
             } else {
                 mPasswordView.setError(getString(R.string.error_incorrect_password));
